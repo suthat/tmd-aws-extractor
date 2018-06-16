21c0928952151a369448df2be418f4df3fb3ec67
suthat <suthat@do.in.th>
2018-06-16 15:53:58 +0700

    updated extractor that can collect files from directory

--- src/com/dointhai/rezonance/Extractor.java
+++ src/com/dointhai/rezonance/Extractor.java
@@ -1,8 +1,11 @@
 package com.dointhai.rezonance;
 
 import com.dointhai.rezonance.bean.AWSData;
-import com.dointhai.rezonance.util.DOMExplorer;
 import com.dointhai.rezonance.util.Reader;
+import com.dointhai.rezonance.util.DOMExplorer;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
@@ -11,8 +14,25 @@ import java.util.ArrayList;
  */
 public class Extractor {
     
+    private static final String targetDirPath = "/Users/suthatronglong/Desktop/tmd-aws-chan";
+    
     public static void main(String args[]) {
-        Reader reader = new Reader("/Users/suthatronglong/Desktop/reports_minute_2010-01-01 00_01_2010-01-01 24_00.xls");
+        try {
+            Files.list(Paths.get(targetDirPath))
+                    .filter(Files::isRegularFile)
+                    .forEach((file) -> {
+                        System.out.println(file.getFileName());
+                        extractorToObject(targetDirPath + "/" + file.getFileName());
+                    });
+        }catch(IOException e) {
+            System.out.println("FileNotFoundException >>> " + e.getMessage());
+        }catch(Exception e) {
+            System.out.println("Exception >>> " + e.getMessage());
+        }
+    }
+    
+    public static void extractorToObject(String pathToFile) {
+        Reader reader = new Reader(pathToFile);
         reader.readAllLines();
         StringBuffer content = reader.getContentAfterReading();
         //System.out.println(content);
